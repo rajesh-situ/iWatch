7270fd9d3e71ca96418346441fd62f5c39ba8f57
Rajesh Panda <rajesh.situ@gmail.com>
2018-07-25 12:14:17 +0530

    calc app initial draft

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Rajesh Panda on 25/07/2018.
+//  Copyright © 2018 Rajesh Panda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.green
+    }
+
+}
+
---
+++ Calculator/calc Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  calc Extension
+//
+//  Created by Rajesh Panda on 25/07/2018.
+//  Copyright © 2018 Rajesh Panda. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
